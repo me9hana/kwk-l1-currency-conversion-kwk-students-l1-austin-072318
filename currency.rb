dd834f7deebe01c27dcee2745cd73d746a5f4307
me9hana <meghanagoli@gmail.com>
2018-07-30 04:06:59 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,38 +1,38 @@
-def usd_to_eur(usd_num = 1)
+# def usd_to_eur(usd_num = 1)
   
-  puts "How many dollars do you want to convert to euros?"
+#   puts "How many dollars do you want to convert to euros?"
  
-  usd_num = gets.chomp.to_i
-  eur_amount = usd_num*0.86
+#   usd_num = gets.chomp.to_i
+#   eur_amount = usd_num*0.86
  
-  puts "#{usd_num} is equal to #{eur_amount}."
+#   puts "#{usd_num} is equal to #{eur_amount}."
  
-end
-usd_to_eur
+# end
+# usd_to_eur
 
-def eur_to_usd(eur_num = 1)
+# def eur_to_usd(eur_num = 1)
   
-  puts "How many euros do you want to convert to dollars?"
+#   puts "How many euros do you want to convert to dollars?"
 
-  eur_num = gets.chomp.to_i
-  usd_amount = eur_num*1.17
+#   eur_num = gets.chomp.to_i
+#   usd_amount = eur_num*1.17
 
-  puts "#{eur_num} is equal to #{usd_amount}."
+#   puts "#{eur_num} is equal to #{usd_amount}."
  
-end
-eur_to_usd
+# end
+# eur_to_usd
 
-def usd_to_inr(usd_num = 1)
+# def usd_to_inr(usd_num = 1)
   
-  puts "How many dollars do you want to convert to rupees?"
+#   puts "How many dollars do you want to convert to rupees?"
   
-  usd_num = gets.chomp.to_i
-  inr_amount = usd_num*68.76
+#   usd_num = gets.chomp.to_i
+#   inr_amount = usd_num*68.76
   
-  puts "#{usd_num} is equal to #{inr_amount}."
+#   puts "#{usd_num} is equal to #{inr_amount}."
   
-end
-usd_to_inr
+# end
+# usd_to_inr
 
 puts "How much money do you want to convert?"
 amount = gets.chomp().to_i
