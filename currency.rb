2baccaf04d50a46d82231ec745625e76c336b057
me9hana <meghanagoli@gmail.com>
2018-07-30 03:42:32 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,11 +1,11 @@
-def usd_to_eur(usd_amount = usd_num/0.86)
+def usd_to_eur(eur_amount = usd_num*(0.86/1)
  
 
  
  
 end
 
-def eur_to_usd(eur_amount = 1.17)
+def eur_to_usd(usd_amount = eur_num1.17)
  
  
 end
