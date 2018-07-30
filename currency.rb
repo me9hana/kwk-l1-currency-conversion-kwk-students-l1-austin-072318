4a39a20560a682bbb3373f65d56291e7c5209b57
me9hana <meghanagoli@gmail.com>
2018-07-30 04:05:12 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -39,13 +39,9 @@ amount = gets.chomp().to_i
  
 puts "Pick from the following menu:"
 puts "1. USD to EUR"
-puts "2. USD to JPY"
-puts "3. USD to GBP"
-puts "4. USD to AUD"
-puts "5. EUR to USD"
-puts "6. JPY to USD"
-puts "7. GBP to USD"
-puts "8. AUD to USD"
+puts "2. EUR to USD"
+puts "3. USD to INR"
+
  
 choice = gets.chomp().to_i
  
