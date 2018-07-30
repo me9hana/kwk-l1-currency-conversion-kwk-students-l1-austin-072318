83ab1236bccf121beb3aca9b04de56b6e1b0c71f
me9hana <meghanagoli@gmail.com>
2018-07-30 04:04:30 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -33,4 +33,40 @@ def usd_to_inr(usd_num = 1)
   
 end
 usd_to_inr
+
+puts "How much money do you want to convert?"
+amount = gets.chomp().to_i
+ 
+puts "Pick from the following menu:"
+puts "1. USD to EUR"
+puts "2. USD to JPY"
+puts "3. USD to GBP"
+puts "4. USD to AUD"
+puts "5. EUR to USD"
+puts "6. JPY to USD"
+puts "7. GBP to USD"
+puts "8. AUD to USD"
+ 
+choice = gets.chomp().to_i
+ 
+case choice
+when 1
+  
+when 2
+ 
+when 3
+  
+when 4
+  
+when 5
+  
+when 6
+  
+when 7
+  
+when 8
+  
+else
+  puts "Invalid input, exiting..."
+end
   
