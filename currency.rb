308843ddf55fa3cca3beaf7d326fbb4e9a33cafc
me9hana <meghanagoli@gmail.com>
2018-07-30 03:53:53 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,4 +1,4 @@
-def usd_to_eur(usd_num)
+def usd_to_eur(usd_num = 1)
   eur_amount = usd_num*0.86
   
 puts "How many dollars do you want to convert to euros?"
@@ -8,7 +8,7 @@ usd_num = gets.chomp.to_i
 puts "#{usd_num} is equal to #{eur_amount}."
  
 end
-usd_to_eur(5)
+usd_to_eur
 
 def eur_to_usd(eur_num)
   usd_amount = eur_num*1.17
