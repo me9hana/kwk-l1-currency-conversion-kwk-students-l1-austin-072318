c099f5dd1daf12d2b9a0b73e1c66b9e5a969017f
me9hana <meghanagoli@gmail.com>
2018-07-30 03:37:59 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,11 +1,11 @@
 def usd_to_eur(usd1 = 0.86)
- puts "How many do you want to convert to EUR?"
+ 
 
  
  
 end
 
 def eur_to_usd(eur1 = 1.17)
-  puts "How many do you want to convert to USD?"
+ 
  
 end
